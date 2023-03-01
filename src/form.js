3d3ac809f6b9b9a2b64cf4342d5030bd10aeef99
Your Name <you@example.com>
2023-03-01 20:21:31 +0530

    changed

--- src/form.js
+++ src/form.js
@@ -2,6 +2,7 @@ import react from "react"
 import {useForm} from "react-hook-form";
 import * as yup from "yup";
 import {yupResolver} from "@hookform/resolvers/yup"
+import { useState } from "react";
 import YupPassword from 'yup-password';
 YupPassword(yup);
 
@@ -10,22 +11,64 @@ const Schema = yup.object().shape({
     firstname : yup.string().required("firstname is mandatory"),
     lastname : yup.string().required("lastname is mandatory"),
     email : yup.string().email("enter valid email").required("enter email"),
-    age : yup.number().min(5, "minimum age  is 5").max(50, "Maximum age is 50").required("age is mandatory").typeError('age is mandatory'),
+    age : yup.number().min(5, "minimum age  is 5").max(50, "Maximum age is 50").required().typeError('age is mandatory'),
     date : yup.string().required("date is mandatory"),
-    password : yup.string().password().required("password is mandatory").min(8,"Minimum 8 chars").max(16, "maximum 16 chars"),
+    state : yup.string().required("Select State"),
+    city : yup.string().required("Select City"),
+    password : yup.string().password().required().min(8,"Minimum 8 chars").max(16, "maximum 16 chars"),
     confirmpassword: yup.string().oneOf([yup.ref("password"),null],"password must match")
 });
 
 function Formfn(){
-const {register,handleSubmit,formState : {errors , isValid}} =useForm({
+const {register,handleSubmit,formState : {errors,isValid}} =useForm({
     resolver : yupResolver(Schema),
     mode : "onChange"
 });
 
-
 const onsubmit = (data)=>{
    console.log(data);
 }
+
+ const City =[
+    {
+        id:1,
+        state:"Tamilnadu",
+        city:"Chennai",
+     },
+    {
+        id:2,
+        state:"Tamilnadu",
+        city:"Madurai",
+     },  
+    {
+        id:3,
+        state:"Kerala",
+        city:"Alappuzha",
+     },  
+    {
+        id:4,
+        state:"Kerala",
+        city:"Thrissur",
+     },  
+ ];
+
+ const State =[
+    {
+        id:1,
+        state:"Tamilnadu"
+    },
+    {
+        id:2,
+        state:"Kerala"
+    }
+ ]
+     
+const Statefn =()=>State.map(x => <option key={x.id}>{x.state}</option>)
+
+const [state,setState] =useState("");
+
+const Cityfn = ()=>City.filter(item => state == item.state).map(value => <option key={value.id}>{value.city}</option>)
+
 return(
     <div className="form">
         <h1>Sign Up</h1>
@@ -42,6 +85,16 @@ return(
             <p className="error">{errors.age?.message}</p>
             <input  {...register("date")} type="date" placeholder="dob" className="date"/>
             <p className="error">{errors.date?.message}</p>
+            <select {...register("state")} value={state} onChange={(e)=>setState(e.target.value)}>
+                <option>select state</option>
+                <Statefn/>
+            </select>
+            <p className="error">{errors.state?.message}</p>
+            <select {...register("city")} >
+                <option>select city</option>
+               <Cityfn/>
+            </select>
+            <p className="error">{errors.city?.message}</p>
             <input  {...register("password")} type="password" placeholder="password" />
             <p className="error">{errors.password?.message}</p>
             <input  {...register("confirmpassword")} type="password" placeholder="confirm password" />
@@ -52,10 +105,5 @@ return(
 
     </div>
 )
-
-
-
-
-
 }
 export default Formfn;
